4df0cc98b00e7837c5a3a2d22694c4a136b2965e
Natanael <natanael_magalhaes@msn.com>
2024-08-09 04:27:06 -0300

    update link

--- scripts/generate_table.py
+++ scripts/generate_table.py
@@ -7,9 +7,10 @@ def main():
   files = list(OUTPUTS_PATH.glob('labels_*.pt'))
   variants = sorted([f.name[7:-3] for f in files])
   for v in variants:
-    image = f'<a href="">image_{v}.pt</a>'
-    image_zca = f'<a href="">image_zca_{v}.pt</a>'
-    labels = f'<a href="">labels_{v}.pt</a>'
+    tag_pattern = '<a href="https://github.com/nmcardoso/ds-distillation/raw/main/outputs/{f}">{f}</a>'
+    image = tag_pattern.format(f=f'image_{v}.pt')
+    image_zca = tag_pattern.format(f=f'image_zca_{v}.pt')
+    labels = tag_pattern.format(f=f'labels_{v}.pt')
     print(f'<tr><td>{v}</td><td>{image}</td><td>{image_zca}</td><td>{labels}</td></tr>')
 
 
