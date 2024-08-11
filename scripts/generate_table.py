57a21999044ab4911d0988b7dac44ae3c014e50e
Natanael <natanael_magalhaes@msn.com>
2024-08-11 14:25:54 -0300

    cifar-10

--- scripts/generate_table.py
+++ scripts/generate_table.py
@@ -1,13 +1,13 @@
 from pathlib import Path
 
 ROOT = Path(__file__).parent.parent
-OUTPUTS_PATH = ROOT / 'outputs'
+OUTPUTS_PATH = ROOT / 'outputs' / 'CIFAR10'
 
 def main():
   files = list(OUTPUTS_PATH.glob('labels_*.pt'))
   variants = sorted([f.name[7:-3] for f in files])
   for v in variants:
-    tag_pattern = '<a href="https://github.com/nmcardoso/ds-distillation/raw/main/outputs/{f}">{f}</a>'
+    tag_pattern = '<a href="https://github.com/nmcardoso/ds-distillation/raw/main/outputs/CIFAR10/{f}">{f}</a>'
     image = tag_pattern.format(f=f'images_{v}.pt')
     image_zca = tag_pattern.format(f=f'images_zca_{v}.pt')
     labels = tag_pattern.format(f=f'labels_{v}.pt')
